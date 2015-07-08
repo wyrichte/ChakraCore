f33c9882a95b106533992caf305717fab6b1837c
Curtis Man <curtism@microsoft.com>
2015-07-08 16:08:29 -0700

    FW: ChakraCore: Move EtwTrace GetLoopBodyName to FunctionBody EtwTrace::GetLoopBodyName is used elsewhere other then EtwTrace. Move it into FunctionBody. Merge common part of jsrt.cpp and jsrtcoredeltas.cpp back to jsrtshared.cpp. Stub out debugging setup and #ifdef native serialized code for now. Disable VTUNE_PROFILING if ETW trace is disabled. Fix minor backend build problems. IECAT #: 75136 Code Reviewer: tcare;saagarwa Changelist: 1566359

--- private/bin/Chakra/JsrtShared.cpp
+++ private/bin/Chakra/JsrtShared.cpp
@@ -2741,3 +2741,321 @@ STDAPI_(JsErrorCode) JsSetPromiseContinuationCallback(JsPromiseContinuationCallb
         return JsNoError;
     });
 }
+
+void JsrtOnLoadScript(Js::JavascriptFunction * scriptFunction, Js::Utf8SourceInfo* utf8SourceInfo);
+
+JsErrorCode RunScriptCore(const wchar_t *script, JsSourceContext sourceContext, const wchar_t *sourceUrl, bool parseOnly, JsValueRef *result)
+{
+    Js::JavascriptFunction *scriptFunction;
+    CompileScriptException se;
+
+    JsErrorCode errorCode = ContextAPINoScriptWrapper(
+        [&](Js::ScriptContext * scriptContext) -> JsErrorCode {
+        PARAM_NOT_NULL(script);
+        PARAM_NOT_NULL(sourceUrl);
+
+        
+        SourceContextInfo * sourceContextInfo = scriptContext->GetSourceContextInfo(sourceContext, NULL);
+
+        if (sourceContextInfo == NULL)
+        {
+            sourceContextInfo = scriptContext->CreateSourceContextInfo(sourceContext, sourceUrl, wcslen(sourceUrl), NULL);
+        }
+
+        SRCINFO si = {
+            /* sourceContextInfo   */ sourceContextInfo,
+            /* dlnHost             */ 0,
+            /* ulColumnHost        */ 0,
+            /* lnMinHost           */ 0,
+            /* ichMinHost          */ 0,
+            /* ichLimHost          */ wcslen(script),
+            /* ulCharOffset        */ 0,
+            /* mod                 */ kmodGlobal,
+            /* grfsi               */ 0
+        };
+
+        Js::Utf8SourceInfo* utf8SourceInfo;
+        scriptFunction = scriptContext->LoadScript(script, &si, &se, result != NULL, false, false, &utf8SourceInfo, Js::Constants::GlobalCode);
+
+        JsrtOnLoadScript(scriptFunction, utf8SourceInfo);
+       
+        return JsNoError;
+    });
+
+    if (errorCode != JsNoError)
+    {
+        return errorCode;
+    }
+
+    return ContextAPIWrapper<false>([&](Js::ScriptContext* scriptContext) -> JsErrorCode {
+        if (scriptFunction == NULL)
+        {
+            HandleScriptCompileError(scriptContext, &se);
+            return JsErrorScriptCompile;
+        }
+
+        if (parseOnly)
+        {
+            PARAM_NOT_NULL(result);
+            *result = scriptFunction;
+        }
+        else
+        {
+            Js::Arguments args(0, NULL);
+#ifdef ENABLE_DEBUG_CONFIG_OPTIONS
+            Js::Var varThis;
+            if (PHASE_FORCE1(Js::EvalCompilePhase))
+            {
+                varThis = Js::JavascriptOperators::OP_GetThis(scriptContext->GetLibrary()->GetUndefined(), kmodGlobal, scriptContext);
+                args.Info.Flags = (Js::CallFlags)CallFlags_Eval;
+                args.Info.Count = 1;
+                args.Values = &varThis;
+            }
+#endif
+            Js::Var varResult = Js::JavascriptFunction::CallFunction<true>(scriptFunction, scriptFunction->GetEntryPoint(), args);
+            if (result != NULL)
+            {
+                *result = varResult;
+            }
+        }
+        return JsNoError;
+    });
+}
+
+STDAPI_(JsErrorCode) JsParseScript(const wchar_t * script, JsSourceContext sourceContext, const wchar_t *sourceUrl, JsValueRef * result)
+{
+    return RunScriptCore(script, sourceContext, sourceUrl, true, result);
+}
+
+STDAPI_(JsErrorCode) JsRunScript(const wchar_t * script, JsSourceContext sourceContext, const wchar_t *sourceUrl, JsValueRef * result)
+{
+    return RunScriptCore(script, sourceContext, sourceUrl, false, result);
+}
+
+JsErrorCode JsSerializeScriptCore(const wchar_t *script, BYTE *functionTable, int functionTableSize, unsigned char *buffer, unsigned long *bufferSize, bool serializeNative)
+{
+    Js::JavascriptFunction *function;
+    CompileScriptException se;
+
+    JsErrorCode errorCode = ContextAPINoScriptWrapper([&](Js::ScriptContext *scriptContext) -> JsErrorCode {
+        PARAM_NOT_NULL(script);
+        PARAM_NOT_NULL(bufferSize);
+
+        if (*bufferSize > 0)
+        {
+            PARAM_NOT_NULL(buffer);
+            ZeroMemory(buffer, *bufferSize);
+        }
+
+        if (scriptContext->IsInDebugMode())
+        {
+            return JsErrorCannotSerializeDebugScript;
+        }
+
+        SourceContextInfo * sourceContextInfo = scriptContext->GetSourceContextInfo(JS_SOURCE_CONTEXT_NONE, NULL);
+        Assert(sourceContextInfo != nullptr);
+
+        SRCINFO si = {
+            /* sourceContextInfo   */ sourceContextInfo,
+            /* dlnHost             */ 0,
+            /* ulColumnHost        */ 0,
+            /* lnMinHost           */ 0,
+            /* ichMinHost          */ 0,
+            /* ichLimHost          */ wcslen(script),
+            /* ulCharOffset        */ 0,
+            /* mod                 */ kmodGlobal,
+            /* grfsi               */ 0
+        };
+
+        Js::Utf8SourceInfo* sourceInfo;
+        function = scriptContext->LoadScript(script, &si, &se, true, true, serializeNative, &sourceInfo, Js::Constants::GlobalCode);
+        return JsNoError;
+    });
+
+    if (errorCode != JsNoError)
+    {
+        return errorCode;
+    }
+
+    return ContextAPIWrapper<false>([&](Js::ScriptContext* scriptContext) -> JsErrorCode {
+        if (function == NULL)
+        {
+            HandleScriptCompileError(scriptContext, &se);
+            return JsErrorScriptCompile;
+        }
+        // Could we have a deserialized function in this case?
+        // If we are going to seralize it, a check isn't to expensive
+        if (CONFIG_FLAG(ForceSerialized) && function->GetFunctionProxy() != null) {
+            function->GetFunctionProxy()->EnsureDeserialized();
+        }
+        Js::FunctionBody *functionBody = function->GetFunctionBody();
+        const Js::Utf8SourceInfo *sourceInfo = functionBody->GetUtf8SourceInfo();
+        DWORD dwSourceCodeLength = sourceInfo->GetCbLength(L"JsSerializeScript");
+        LPCUTF8 utf8Code = sourceInfo->GetSource(L"JsSerializeScript");
+
+        BEGIN_TEMP_ALLOCATOR(tempAllocator, scriptContext, L"ByteCodeSerializer");
+        HRESULT hr = S_OK;
+#ifdef ENABLE_NATIVE_CODE_SERIALIZATION
+        if (serializeNative)
+        {
+            BYTE *nativeCode = nullptr;
+            DWORD dwNativeCodeSize = 0;
+            BYTE *byteCodeBuffer = nullptr;
+            DWORD dwByteCodeSize = 0;
+            hr = Js::ByteCodeSerializer::SerializeToBuffer(scriptContext, tempAllocator, dwSourceCodeLength, utf8Code, 0, nullptr, functionBody, functionBody->GetHostSrcInfo(), true, &byteCodeBuffer, &dwByteCodeSize, GENERATE_BYTE_CODE_FOR_NATIVE);
+            if (SUCCEEDED(hr))
+            {
+                GenerateAllFunctionsForSerialization(scriptContext->GetNativeCodeGenerator(), functionBody, nullptr, 0, byteCodeBuffer, dwByteCodeSize, functionTableSize, functionTable, &nativeCode, &dwNativeCodeSize);
+
+                if (*bufferSize >= dwNativeCodeSize)
+                {
+                    memcpy_s(buffer, *bufferSize, nativeCode, dwNativeCodeSize);
+                }
+                else if (buffer != nullptr)
+                {
+                    return JsErrorInvalidArgument;
+                }
+
+                *bufferSize = dwNativeCodeSize;
+                CoTaskMemFree(nativeCode);
+                CoTaskMemFree(byteCodeBuffer);
+            }
+        }
+        else
+#else
+        Assert(!serializeNative);
+#endif
+        {
+            hr = Js::ByteCodeSerializer::SerializeToBuffer(scriptContext, tempAllocator, dwSourceCodeLength, utf8Code, 0, nullptr, functionBody, functionBody->GetHostSrcInfo(), false, &buffer, bufferSize);
+        }
+        END_TEMP_ALLOCATOR(tempAllocator, scriptContext);
+
+        if (SUCCEEDED(hr))
+        {
+            return JsNoError;
+        }
+        else
+        {
+            return JsErrorScriptCompile;
+        }
+    });
+
+}
+
+STDAPI_(JsErrorCode) JsSerializeScript(const wchar_t *script, unsigned char *buffer, unsigned long *bufferSize)
+{
+    return JsSerializeScriptCore(script, nullptr, 0, buffer, bufferSize, Js::Configuration::Global.flags.IncludeNativeCodeWithSerializedByteCodes);
+}
+
+#ifdef ENABLE_NATIVE_CODE_SERIALIZATION
+STDAPI_(JsErrorCode) JsSerializeNativeScript(const wchar_t *script, BYTE *functionTable, int functionTableSize, unsigned char *buffer, unsigned long *bufferSize)
+{
+    return JsSerializeScriptCore(script, functionTable, functionTableSize, buffer, bufferSize, true);
+}
+#endif
+
+JsErrorCode RunSerializedScriptCore(const wchar_t *script, unsigned char *buffer, JsSourceContext sourceContext, const wchar_t *sourceUrl, bool parseOnly, JsValueRef *result)
+{
+    Js::JavascriptFunction *function;
+    JsErrorCode errorCode = ContextAPINoScriptWrapper([&](Js::ScriptContext *scriptContext) -> JsErrorCode {
+        PARAM_NOT_NULL(script);
+        PARAM_NOT_NULL(buffer);
+        PARAM_NOT_NULL(sourceUrl);
+
+        size_t length = wcslen(script);
+        if (length > UINT_MAX)
+        {
+            return JsErrorOutOfMemory;
+        }
+
+        size_t cbUtf8Buffer = (length + 1) * 3;
+        if (cbUtf8Buffer > UINT_MAX)
+        {
+            return JsErrorOutOfMemory;
+        }
+
+        LPUTF8 utf8Script = RecyclerNewArrayLeaf(scriptContext->GetRecycler(), utf8char_t, cbUtf8Buffer);
+
+        Assert(length < MAXLONG);
+        utf8::EncodeIntoAndNullTerminate(utf8Script, script, static_cast<charcount_t>(length));
+        
+        SourceContextInfo *sourceContextInfo;
+        SRCINFO *hsi;
+        Js::FunctionBody *functionBody = NULL;
+
+        HRESULT hr;
+
+        sourceContextInfo = scriptContext->GetSourceContextInfo(sourceContext, NULL);
+
+        if (sourceContextInfo == NULL)
+        {
+            sourceContextInfo = scriptContext->CreateSourceContextInfo(sourceContext, sourceUrl, wcslen(sourceUrl), NULL);
+        }
+
+        SRCINFO si = {
+            /* sourceContextInfo   */ sourceContextInfo,
+            /* dlnHost             */ 0,
+            /* ulColumnHost        */ 0,
+            /* lnMinHost           */ 0,
+            /* ichMinHost          */ 0,
+            /* ichLimHost          */ wcslen(script),
+            /* ulCharOffset        */ 0,
+            /* mod                 */ kmodGlobal,
+            /* grfsi               */ 0
+        };
+
+        ulong flags = 0;
+
+        if (CONFIG_FLAG(CreateFunctionProxy) && !scriptContext->IsProfiling())
+        {
+            flags = fscrAllowFunctionProxy;
+        }
+
+        hsi = scriptContext->AddHostSrcInfo(&si);
+        hr = Js::ByteCodeSerializer::DeserializeFromBuffer(scriptContext, flags, utf8Script, hsi, buffer, nullptr, &functionBody);
+
+        if (FAILED(hr))
+        {
+            return JsErrorBadSerializedScript;
+        }
+
+        function = scriptContext->GetLibrary()->CreateScriptFunction(functionBody);
+
+        JsrtOnLoadScript(function, functionBody->GetUtf8SourceInfo());
+
+        return JsNoError;
+    });
+
+    if (errorCode != JsNoError)
+    {
+        return errorCode;
+    }
+
+    return ContextAPIWrapper<false>([&](Js::ScriptContext* scriptContext) -> JsErrorCode {
+        if (parseOnly)
+        {
+            PARAM_NOT_NULL(result);
+            *result = function;
+        }
+        else
+        {
+            Js::Var varResult = Js::JavascriptFunction::CallFunction<true>(function, function->GetEntryPoint(), Js::Arguments(0, NULL));
+            if (result != NULL)
+            {
+                *result = varResult;
+            }
+        }
+        return JsNoError;
+    });
+}
+
+
+STDAPI_(JsErrorCode) JsParseSerializedScript(const wchar_t * script, unsigned char *buffer, JsSourceContext sourceContext, const wchar_t *sourceUrl, JsValueRef * result)
+{
+    return RunSerializedScriptCore(script, buffer, sourceContext, sourceUrl, true, result);
+}
+
+STDAPI_(JsErrorCode) JsRunSerializedScript(const wchar_t * script, unsigned char *buffer, JsSourceContext sourceContext, const wchar_t *sourceUrl, JsValueRef * result)
+{
+    return RunSerializedScriptCore(script, buffer, sourceContext, sourceUrl, false, result);
+}
