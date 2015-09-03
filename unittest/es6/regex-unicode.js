c17ecd0cb0474b368a54b46c8be54ae907e8537d
Gorkem Yakin <goyakin@microsoft.com>
2015-09-02 20:51:49 -0700

    Tests for "Negate code points in supplementary planes" in core

---
+++ unittest/es6/regex-unicode.js
@@ -0,0 +1,53 @@
+if (this.WScript && this.WScript.LoadScriptFile) {
+    this.WScript.LoadScriptFile("..\\..\\core\\test\\UnitTestFramework\\UnitTestFramework.js");
+}
+
+function assertTest(asserter, re, string, message) {
+    asserter(re.test(string), message);
+}
+
+
+function assertMatches() {
+    assertTest(assert.isTrue, ...arguments);
+}
+
+function assertDoesNotMatch(re, string, message) {
+    assertTest(assert.isFalse, ...arguments);
+}
+
+// TODO: RegExp functions currently process strings as a list of code units as opposed to a list of code
+//       points. This causes a RegExp to match just the high surrogate. For example, /[^\ud800\udc00]/
+//       matches "\ud800". Some of the patterns below have the format "^...$" to force the RegExp to match
+//       the string fully. Once the bug is fixes, the '^'s and '$'s can be removed. The bug # is 3679792.
+var tests = [
+    {
+        name: "A character set containing a negated character from a supplementary plane shouldn't match the character itself",
+        body: function () {
+            assertDoesNotMatch(/^[^\ud800\udc00]$/u, "\ud800\udc00", "Surrogate pair in RegExp and surrogate pair in string to test");
+            assertDoesNotMatch(/^[^\ud800\udc00]$/u, "\u{10000}", "Surrogate pair in RegExp and code point in string to test");
+            assertDoesNotMatch(/^[^\u{10000}]$/u, "\ud800\udc00", "Code point in RegExp and surrogate pair in string to test");
+            assertDoesNotMatch(/^[^\u{10000}]$/u, "\u{10000}", "Code point in RegExp and code point in string to test");
+        }
+    },
+    {
+        name: "A character set containing a negated character from a supplementary plane should match other characters",
+        body: function () {
+            assertMatches(/^[^\ud800\udc00]$/u, "\ud801\udc01", "Surrogate pair in RegExp and surrogate pair in string to test");
+            assertMatches(/^[^\u{10000}]$/u, "\ud801\udc01", "Surrogate pair in RegExp and code point in string to test");
+            assertMatches(/^[^\ud800\udc00]$/u, "\u{10101}", "Code point in RegExp and surrogate pair in string to test");
+            assertMatches(/^[^\u10000]$/u, "\u{10101}", "Code point in RegExp and code point in string to test");
+
+            assertMatches(/^[^\u10000]$/u, "\u0345", "Code point in RegExp and code unit in string to test");
+            assertMatches(/^[^\ud800\udc00]$/u, "\u0345", "Surrorgate pair in RegExp and code unit in string to test");
+        }
+    },
+    {
+        name: "A character set containing a negated character from the basic plane should match characters from supplementary planes",
+        body: function () {
+            assertMatches(/^[^0345]$/u, "\ud800\udc00", "Surrogate pair");
+            assertMatches(/^[^0345]$/u, "\u{10000}", "Code point");
+        }
+    },
+];
+
+testRunner.runTests(tests, { verbose: WScript.Arguments[0] != "summary" });
