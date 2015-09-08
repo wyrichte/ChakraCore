4721cd7ca2e5556bdb629da708993ad70d45ac74
Tom Care <tcare@microsoft.com>
2015-09-08 22:00:41 +0000

    Fix bad merge

--- private/lib/SCA/SCAEngine.cpp
+++ private/lib/SCA/SCAEngine.cpp
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-//---------------------------------------------------------------------------
-// Copyright (C) Microsoft. All rights reserved.
-//----------------------------------------------------------------------------
-
-#include "SCAPch.h"
-#include "Debug\ProbeContainer.h"
-#include "Debug\DebugContext.h"
-
-namespace Js
-{
-    void ScriptContextHolder::ThrowIfFailed(HRESULT hr) const
-    {
-        if (FAILED(hr))
-        {
-            if (GetScriptContext()->IsInDebugMode())
-            {
-                JavascriptExceptionObject *exception = null;
-                try
-                {
-                    GetScriptContext()->GetDebugContext()->GetProbeContainer()->SetThrowIsInternal(true);
-                    HostDispatch::HandleDispatchError(GetScriptContext(), hr, NULL);
-                }
-                catch (JavascriptExceptionObject *exception_)
-                {
-                    Assert(exception_);
-                    GetScriptContext()->GetDebugContext()->GetProbeContainer()->SetThrowIsInternal(false);
-                    exception = exception_;
-                }
-
-                if (exception)
-                {
-                    // Rethrow the exception again.
-                    throw exception;
-                }
-            }
-            else
-            {
-                HostDispatch::HandleDispatchError(GetScriptContext(), hr, NULL);
-            }
-        }
-    }
-
-=======
 //---------------------------------------------------------------------------
 // Copyright (C) Microsoft. All rights reserved.
 //----------------------------------------------------------------------------
@@ -83,6 +39,4 @@ namespace Js
             }
         }
     }
-
->>>>>>> SDToGit
 };
