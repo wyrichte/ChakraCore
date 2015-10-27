d815576b603fa3385147abb778d8d3d0a307ea96
Louis Lafreniere <louisl@microsoft.com>
2015-10-27 16:57:39 -0700

    ChakraDiag

--- private/bin/ChakraDiag/RuntimeCore.h
+++ private/bin/ChakraDiag/RuntimeCore.h
@@ -398,6 +398,10 @@ class ScriptContextTelemetry;
 #define IsJsDiag
 #include "CompoundString.h"
 #include "CompoundString.cpp"
+#ifdef _M_X64_OR_ARM64
+// TODO: Clean this warning up
+#pragma warning(disable:4267) // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#endif
 #undef IsJsDiag
 #include "BufferStringBuilder.h"
 #include "LiteralStringObject.h"        // Needed by JavascriptStringObject DAC
