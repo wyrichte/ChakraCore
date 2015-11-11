9c732047bb7c3bd5e72e3bf964777b49ba705e84
Curtis Man <curtism@microsoft.com>
2015-11-10 21:39:37 -0800

    Expand the file description suffix to specify debug vs test and private vs pre-release
    Also have release build be specified by CommonDefine.h instead of build properties.
    Have release build number be always 0 and qfe be statically specify in the commondefines.h
    
    Add submodule comment message to change.txt

--- Build/scripts/pre_build.ps1
+++ Build/scripts/pre_build.ps1
@@ -12,4 +12,29 @@ $OutterScriptRoot = $PSScriptRoot;
 
 & $PSScriptRoot\..\..\core\Build\Scripts\pre_build.ps1 -oauth $oauth -srcpath $srcpath -binpath $binpath -objpath $objpath
 
+$exitCode = $LastExitCode;
+
+if (Test-Path Env:\TF_BUILD_SOURCEGETVERSION)
+{
+    $commitHash = ($Env:TF_BUILD_SOURCEGETVERSION).split(':')[2];
+    $gitExe = GetGitPath;
+    
+    $outputDir = $Env:TF_BUILD_DROPLOCATION
+    $outputFile = Join-Path -Path $outputDir -ChildPath "change.txt"
+
+    Push-Location $srcpath;
+    $summary = iex "$gitExe diff --submodule $commitHash~1..$commitHash core"
+    Pop-Location
+    if ($summary)
+    {
+        Push-Location "$srcpath\core"
+        $commits = $summary[0].split()[2].replace(":","");
+        Write-Output "" | Out-File $outputFile -Append
+        Write-Output "=======================================" | Out-File $outputFile -Append
+        Write-Output "Core Commits: $commits" | Out-File $outputFile -Append
+        Write-Output "" | Out-File $outputFile -Append
+        (iex "$gitExe log --name-status $commits") | Out-File $outputFile -Append
+        Pop-Location
+    }
+}
 
