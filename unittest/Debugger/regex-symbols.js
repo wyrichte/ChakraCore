a40c612a0ed88139c95519c821ccc1ff04633abe
Gorkem Yakin <goyakin@microsoft.com>
2016-02-10 09:35:45 -0800

    Show String.prototype.search in the debugger call stack
    This change tags `String.prototype.search` so that it shows up in the debugger call stack when it delegates the work to `RegExp.prototype[@@search]`.

--- unittest/Debugger/regex-symbols.js
+++ unittest/Debugger/regex-symbols.js
@@ -1,3 +1,9 @@
+RegExp.prototype[Symbol.search] = function search() {
+    return undefined; /**bp:stack()**/
+}
+
+'string'.search(/./);
+
 RegExp.prototype[Symbol.match] = function match() {
     return undefined; /**bp:stack()**/
 }
