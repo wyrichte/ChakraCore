9f9c471d7c9fb40fbdfe2db024b6a3536e0c4cc0
Akrosh Gandhi <akroshg@microsoft.com>
2016-09-26 15:38:02 -0700

    Revert "commenting SCA_SharedArrayBuffer SCA_Sharable code to resolve the build error."
    This reverts commit 5274e8c66b784c50284982c8b8536e746f0337dc.

--- private/lib/SCALookup/SCAWalk.cpp
+++ private/lib/SCALookup/SCAWalk.cpp
@@ -180,7 +180,7 @@ HRESULT SkipWalker::WalkObject(_In_ const StreamReader& reader, _In_ SCATypeId t
         break;
 
     case SCA_ArrayBuffer:
-    //case SCA_SharedArrayBuffer:
+    case SCA_SharedArrayBuffer:
         hr = WalkByteArrayData(reader);
         break;
 
