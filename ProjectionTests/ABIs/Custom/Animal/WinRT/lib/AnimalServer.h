c97ba5b928d6e04774bdc8adb6134ded916bf135
Kyle Farnung <kfarnung@microsoft.com>
2017-04-22 17:22:40 -0700

    Fixing build break in AnimalServer.h
    A comma was missing between parameters.

--- ProjectionTests/ABIs/Custom/Animal/WinRT/lib/AnimalServer.h
+++ ProjectionTests/ABIs/Custom/Animal/WinRT/lib/AnimalServer.h
@@ -762,7 +762,7 @@ namespace Animals
             return S_OK;
         }
 
-        IFACEMETHOD(TestBug8327782_StackArguments)(HSTRING str1, HSTRING str2, HSTRING str3, HSTRING str4, HSTRING str5, HSTRING str6, HSTRING str7, HSTRING str8, HSTRING str9 HSTRING* result)
+        IFACEMETHOD(TestBug8327782_StackArguments)(HSTRING str1, HSTRING str2, HSTRING str3, HSTRING str4, HSTRING str5, HSTRING str6, HSTRING str7, HSTRING str8, HSTRING str9, HSTRING* result)
         {
             // Do non-trivial work to ensure that the stack/registers are touched
             WindowsConcatString(str1, str2, result);
