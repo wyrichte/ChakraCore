49c625bdf31cfff8179688cb4e334cbc29af1548
Jianchun Xu <jianxu@microsoft.com>
2017-06-29 09:50:28 -0700

    [MERGE #3239 @jianchun] move recycler verify mark false reference checks to runtime
    Merge pull request #3239 from jianchun:falseref
    
    The checks were in Common.Memory and had no access to runtime headers. We had
    to hard code runtime data field offsets. This was inconvenient and unstable.
    
    This change moves the checks to runtime so that we can use exact field offsets
    with "offsetof".
    
    Added one more GC false reference filter (native array elements).
    
    Added one unused field initialization to avoid false reference.

--- private/bin/GCHost/GCHost.cpp
+++ private/bin/GCHost/GCHost.cpp
@@ -522,6 +522,14 @@ bool GetDeviceFamilyInfo(
     return false;
 }
 
+#if DBG && defined(RECYCLER_VERIFY_MARK)
+bool IsLikelyRuntimeFalseReference(char* objectStartAddress, size_t offset,
+    const char* typeName)
+{
+    return false;
+}
+#endif
+
 //////////////////// End test stubs ////////////////////
 
 //////////////////// Begin program entrypoint ////////////////////
