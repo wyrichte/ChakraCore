25a033c45c8c4899b11e41e47652cf58f1757abc
Matt Gardner <magardn@microsoft.com>
2017-10-05 10:52:42 -0700

    add optional copy to share for arm64 builds

--- private/bin/jshost/postbuild.cmd
+++ private/bin/jshost/postbuild.cmd
@@ -12,13 +12,22 @@ setlocal
 set _BuildArch=%1
 set _OutputDir=%~f2
 set _IELibsDir=%~f3
+set _RemoteDir=%~f4
 
 :: Copy PDM files to jshost.exe.local in build output directory
-set _PdmFiles=%_IELibsDir%\pdm\%_BuildArch%\*.dll
+set _PdmFiles=%_IELibsDir%\pdm\%_BuildArch%
 set _TargetDir=%_OutputDir%jshost.exe.local
 
 mkdir "%_TargetDir%"
 
-echo Copying "%_PdmFiles%" to "%_TargetDir%"
-echo.
-copy /Y "%_PdmFiles%" "%_TargetDir%"
+echo Robocopy *.dll from "%_PdmFiles%" to "%_TargetDir%"
+%WinDir%\System32\robocopy /NS /NC /NFL /NDL /NP /NJS /NJH /E *.dll "%_PdmFiles%" "%_TargetDir%"
+
+if not [%_RemoteDir%]==[] (
+    if "%_BuildArch%" EQU "ARM64" (
+        echo Robocopy from "%_OutputDir%" to "%_RemoteDir%"
+        %WinDir%\System32\robocopy /NS /NC /NFL /NDL /NP /NJS /NJH /E * %_OutputDir% %_RemoteDir%
+    )
+)
+
+exit /b 0
