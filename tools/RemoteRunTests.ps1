af905779fc02be22cb44448733472a584763b958
Derek Morris <demorri@microsoft.com>
2017-11-06 11:19:25 -0800

    Update arm64 test runner
    Now runs rl on all working dirs.

--- tools/RemoteRunTests.ps1
+++ tools/RemoteRunTests.ps1
@@ -2,8 +2,10 @@
 # grabs your arm64_debug build for the tests
 
 param(
-[string]$binflavor="arm64_debug",
-[switch]$showSkips=$false
+    [string]$binflavor="arm64_debug",
+    [switch]$showSkips=$false,
+    [switch]$createcsv=$false,
+    [switch]$debugOut=$false
 );
 $configpath = (Join-Path (Join-Path $Env:UserProfile "ChakraDevConfig") "Chakra.Build.user.props");
 
@@ -15,8 +17,8 @@ catch [System.Management.Automation.ItemNotFoundException]
 {
     echo "Didn't find a Chakra remote development config!";
     echo "Try making $configpath with the following template (replace the machine name with your target machine and the share with a new share):";
-echo "    <?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+echo "    <?xml version=`"1.0`" encoding=`"utf-8`"?>
+<Project ToolsVersion=`"15.0`" xmlns=`"http://schemas.microsoft.com/developer/msbuild/2003`">
   <PropertyGroup>
     <ARMRemoteMachineName>chakracobaltX</ARMRemoteMachineName>
     <ARMRemoteShare>\\`$(ARMRemoteMachineName)\DevelopmentBinaries</ARMRemoteShare>
@@ -38,22 +40,28 @@ if(!(Test-Connection -ComputerName $machineName -Quiet))
 echo "Establishing connection and attempting remote work...";
 try
 {
-    [string]$testdir = Join-Path $machineShare "SunSpiderFunctionality";
+    [string]$testdirbase = Join-Path $machineShare "alltests";
+    [string]$testfullbase = Join-Path $testdirbase "unittests";
+    [string]$testcorebase = Join-Path $testdirbase "tests";
     [string]$bindir = Join-Path $machineShare $binflavor;
     echo "Copying tests...";
-    # Mirror NoJobHeader NoJobSummary NoProgress NoDirectoryList
-    robocopy /MIR /NJH /NJS /NP /NDL $chakraFullDir\unittest\SunSpiderFunctionality $testdir;
+    # Copy all of our tests - this means that there's going to be a couple minutes
+    # of copying files the first time, but that changing test sets is faster later
+    # Flags are Mirror NoJobHeader NoJobSummary NoProgressbar NoDirectoryList
+    robocopy /MIR /NJH /NJS /NP /NDL $chakraFullDir\core\test $testcorebase;
+    robocopy /MIR /NJH /NJS /NP /NDL $chakraFullDir\unittest $testfullbase;
     echo "Copying binaries...";
     robocopy /MIR /NJH /NJS /NP /NDL $chakraFullDir\Build\VcBuild\bin\$binflavor $bindir;
     echo "Invoking test runner...";
-    Invoke-Command -ComputerName $machineName -ArgumentList $machineShare,$bindir,$testdir,$constantSwitches,$showSkips -ErrorAction Stop -ScriptBlock {
-        param([string]$machineShare, [string]$bindir, [string]$testdir, [string]$constantSwitches, [bool]$showskips);
+    Invoke-Command -ComputerName $machineName -ArgumentList $machineShare,$bindir,$testcorebase,$testfullbase,$constantSwitches,$showSkips,$debugOut -ErrorAction Stop -ScriptBlock {
+        param([string]$machineShare, [string]$bindir, [string]$testcorebase, [string]$testfullbase, [string]$constantSwitches, [bool]$showskips, [bool]$debugOut);
         echo "Remote test runner started";
-        # A lot of the arm64 machines have postmortem debuggers set up.
+        # A few of the arm64 machines have postmortem debuggers set up.
         # In general, this is good; however, we hit an issue when doing
         # test runs, as the debugger will get spawned and just hang our
         # run. To work around this, the script will kill windbg when it
         # is running during the test run!
+        echo "Preventing Just-In-Time debugger interference"
         try {
             $currentprocs = Get-Process -Name windbg -ErrorAction Stop;
             # throws exception if there is one
@@ -64,59 +72,8 @@ try
         {
             echo "Windbg not currently running on target, will kill any that spawn during run...";
         }
-        
-        # Now we can get to actually running jshost on these
-        [string]$chakraBin = Join-Path $bindir "jshost.exe";
 
-        # Helper function to run a single test
-        function runTest([string]$testfile, [string]$additionalArgs, [string]$baseline, [REF]$res)
-        {
-            [string]$testFileFullPath = Join-Path -Path $testdir -ChildPath $testfile;
-            $stdoutloc = New-TemporaryFile;
-            $stderrloc = New-TemporaryFile;
-
-            echo "Running `"$chakraBin $constantSwitches $additionalArgs $testFileFullPath`" ...";
-            if([string]::IsNullOrWhiteSpace($additionalArgs))
-            {
-                $additionalArgs = " ";
-            }
-            $Process = Start-Process -FilePath $chakraBin -ArgumentList $constantSwitches,$additionalArgs,$testFileFullPath -RedirectStandardOutput $stdoutloc.FullName -RedirectStandardError $stderrloc.FullName -Wait;
-            
-            [string]$output = Get-Content -Raw -Path $stdoutloc.FullName;
-            [string]$error = Get-Content -Raw -Path $stderrloc.FullName;
-            [int]$exitcode = $Process.ExitCode;
-
-            if(![string]::IsNullOrWhiteSpace($error) -or ($exitcode -ne 0))
-            {
-                echo "Test terminated with exit code $exitcode";
-                echo "stdout:";
-                echo $output;
-                echo "stderr:";
-                echo $error;
-                $res.value = $false;
-                return;
-            }
-
-            if(![string]::IsNullOrWhiteSpace($baseline))
-            {
-                [string]$baselinepath = Join-Path -Path $testdir -ChildPath $baseline;
-                [string]$baselinecontents = Get-Content -Path $baselinepath -Raw;
-                if($baselinecontents -ne $output) {
-                    echo "Test completed without error code, but baselines differ!";
-                    fc.exe $baselinepath $stdoutloc.FullName;
-                    $res.Value = $false;
-                    return;
-                }
-            }
-
-            if([string]::IsNullOrWhiteSpace($error) -and ($exitcode -eq 0))
-            {
-                $res.value = $true;
-                return;
-            }
-        };
-
-        echo "Running selected tests in $testdir";
+        echo "Setting up test run...";
 
         # start job to kill windbg
         $windbgkiller = Start-Job -ScriptBlock {
@@ -142,91 +99,52 @@ try
             }
         }
 
-        # parse rlexe for the flags so we don't have to update them in two places
-        [xml]$rlexe = Get-Content -Path (Join-Path $testdir "rlexe.xml");
-
-        $tests = $rlexe.'regress-exe'.test;
-        $enabledtests = `
-        "3d-morph.js", `
-        "access-binary-trees", `
-        "access-fannkuch.js", `
-        "access-nbody.js", `
-        "access-nsieve.js", `
-        "bitops-3bit-bits-in-byte.js", `
-        "bitops-bits-in-byte.js", `
-        "bitops-bitwise-and.js", `
-        "bitops-nsieve-bits.js", `
-        "crypto-md5.js", `
-        "crypto-sha1.js", `
-        "math-partial-sums.js", `
-        "regexp-dna.js", `
-        "string-base64.js", `
-        "string-fasta.js", `
-        "string-tagcloud.js", `
-        "string-validate-input.js", `
-        "TerminatorDoNotRemoveThisReducesDiffComplexityALittle";
-        [int]$total = 0;
-        [int]$totalpassed = 0;
-        foreach($test in $tests) {
-            [bool]$cond = $false;
-            [string]$file = $test.default.files.Trim();
-            foreach($enabled in $enabledtests)
-            {
-                if($enabled.Trim().ToLower() -eq $file.ToLower())
-                {
-                    $cond = $true;
-                    break;
-                }
-            }
-            if(!$cond)
-            {
-                if($showskips)
-                {
-                    echo "Skipping $file, as it's not selected...";
-                }
-                continue;
-            }
-            [string]$flags = $test.default.'compile-flags';
-            if([string]::IsNullOrWhiteSpace($flags))
-            {
-                $flags = "";
-            }
-            else
-            {
-                $flags = $flags.Trim();
-            }
-            if($flags.ToLower().Contains("testetwdll"))
-            {
-                if($showskips)
-                {
-                    echo "Skipping ETW variant of test, as it is not currently supported...";
-                }
-                continue;
-            }
-            [string]$baseline = $test.default.baseline;
-            if([string]::IsNullOrWhiteSpace($baseline))
-            {
-                $baseline = null;
-            }
-            else
-            {
-                $baseline = $baseline.Trim();
-            }
-            [bool]$res = $false;
-            runTest $file $flags $baseline ([REF]$res);
-            if($res)
-            {
-                $totalpassed = $totalpassed + 1;
-                echo "PASSED";
-            }
-            else
-            {
-                echo "FAILED";
-            }
-            $total = $total + 1;
+        # We need to figure out the local path to the share, because cmd is old
+        [string]$wdtemp = $testfullbase.Replace("\\","");
+        $wdtempsegs = $wdtemp.Split("\");
+        [string]$computername = $wdtempsegs[0];
+        [string]$sharename = $wdtempsegs[1];
+        $shares = Get-WmiObject -Class Win32_Share;
+        $share = $shares | Where-Object { $_.Name -eq $sharename };
+        $workingDirectory = $share.Path;
+        for($i=2; $i -lt $wdtempsegs.Length; $i++)
+        {
+            $workingDirectory = Join-Path $workingDirectory $wdtempsegs[$i];
         }
+
+        if($debugOut)
+        {
+            echo "computername is $computername";
+            echo "sharename is $sharename";
+            echo "workingdirectory is $workingDirectory";
+        }
+
+        # Now we can get to actually running jshost on these
+        [string]$rlbin = Join-Path $testcorebase "runtests.cmd";
+
+        # Arg setup
+        [string]$binaryName = "jshost.exe";
+        [string]$bindirarg = Join-Path $bindir "..";
+        #$failingDirs = "`"" + "Array,AsmJs,AsyncDebug,bailout,Basics,Bugs,crossthread,Date,Debugger,DebuggerCommon,Error,ErrorCommon,es6,FixedFields,Function,HeapEnum,host,iasd,InlineCaches,InternalProfile,Intl,IntlCore,jd,Miscellaneous,msrc,NativeUnitTests,Object,Opegen,Operators,Optimizer,Profiler,sca,StackTrace,strict,Strings,SunSpider,SunSpider1.0.2,SunSpiderFunctionality,UnifiedRegex,V8,V8strict,V8_Functionality" + "`"";
+        [string]$dirs = "`"" + "fieldopts,JSON,KrakenFunctionality,Lib,loop,Math,Regex,typedarray,utf8" + "`"";
+        [string]$extraflags = "`"-oopjit-`"";
+
+        $stdoutloc = New-TemporaryFile;
+        $stderrloc = New-TemporaryFile;
+
+        echo "Starting test run... (output will display all at once at the end)";
+
+        $rlproc = Start-Process -FilePath $rlbin -ArgumentList "-arm64","-debug","-binary",$binaryName,"-bindir",$bindirarg,"-dirs",$dirs,"-extrahostflags",$extraflags -Wait -WorkingDirectory $workingDirectory -RedirectStandardOutput $stdoutloc.FullName -RedirectStandardError $stderrloc.FullName;
+
+        [string]$output = Get-Content -Raw -Path $stdoutloc.FullName;
+        [string]$error = Get-Content -Raw -Path $stderrloc.FullName;
+        [int]$exitcode = $rlproc.ExitCode;
+
+        echo $output;
+        echo $error;
+
+        echo "Re-enabling JIT debugging...";
         Stop-Job $windbgkiller;
-        echo "Passed $totalpassed/$total tests";
     }
 }
 catch [System.Management.Automation.Remoting.PSRemotingTransportException]
@@ -234,3 +152,5 @@ catch [System.Management.Automation.Remoting.PSRemotingTransportException]
     echo "Connection failure - did you run Enable-PSRemoting on the target machine?";
     exit -1;
 }
+echo "Successfully finished";
+exit 0;
