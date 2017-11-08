4470f02f5d4f9b95cb2a38db8da5c139c5ca332b
meg-gupta <megupta@microsoft.com>
2017-11-08 11:47:32 -0800

    [MERGE #4170 @meg-gupta] Record default array iterator functions on the library object
    Merge pull request #4170 from meg-gupta:fixwebcrash
    
    Fixes OS#14525371

---
+++ unittest/host/iteratorDel.js
@@ -0,0 +1,13 @@
+function foo() {
+    var arrProto = Array.prototype;
+    delete arrProto.entries;
+    var obj = CreateDomArrayObject();
+    obj.AddObject(101);
+    obj.AddObject(102);
+    var iter = obj.entries();
+    print(iter);
+}
+
+foo();
+
+
