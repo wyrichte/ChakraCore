90c10d3430a7616009e0f19f7cbbfedab0df103b
Thomas Moore (CHAKRA) <thomasmo@microsoft.com>
2018-01-05 12:17:09 -0800

    [MERGE #4503 @thomasmo] 18-01 Security Update
    Merge pull request #4503 from thomasmo:1801
    
    18-01 Security Update that addresses the following issues in ChakraCore:
    CVE-2018-0758
    CVE-2018-0762
    CVE-2018-0767
    CVE-2018-0768
    CVE-2018-0769
    CVE-2018-0770
    CVE-2018-0772
    CVE-2018-0773
    CVE-2018-0774
    CVE-2018-0775
    CVE-2018-0776
    CVE-2018-0777
    CVE-2018-0778
    CVE-2018-0780
    CVE-2018-0781

---
+++ unittest/msrc/41913.js
@@ -0,0 +1,31 @@
+function inlinee() {
+    return inlinee.arguments[0];
+}
+
+function opt(convert_to_var_array) {
+    // To make the in-place type conversion happen, it must have multiple segments
+    let stack_arr = []; // JavascriptNativeFloatArray
+    stack_arr[10000] = 1.1;
+    stack_arr[20000] = 2.2;
+
+    let heap_arr = inlinee(stack_arr);
+    convert_to_var_array(heap_arr); // on the final call, it's changed to a Var array because of {}
+
+    stack_arr[10000] = 2.3023e-320;  // but, on the final call, it's still written to as if it's a nativefloat
+
+    // this is where bailout occurs (BailOutOnNotNativeArray), because it's trying to read the float value as a Var
+    return heap_arr[10000];
+}
+
+function main() {
+    for (let i = 0; i < 130; i++) {
+        opt(new Function('')); // Prevents inlining
+    }
+
+    var x = opt(function(heap_arr) {
+        heap_arr[10000] = {}; // ConvertToVarArray
+    });
+}
+
+main();
+print("PASSED");
