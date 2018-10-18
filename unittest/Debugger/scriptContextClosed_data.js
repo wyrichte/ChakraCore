426792e6bbf51e5672082dafc382c4aaf7aaf038
Jeffrey Lin <yullin@microsoft.com>
2018-10-18 13:00:47 -0700

    [MERGE #5731 @yullin-ms] Implemented Math min and max as JsBuiltIns
    Merge pull request #5731 from yullin-ms:mathMinMax
    
    Implemented Math min and max as JsBuiltIns

--- unittest/Debugger/scriptContextClosed_data.js
+++ unittest/Debugger/scriptContextClosed_data.js
@@ -6,4 +6,7 @@ var x = [ {toString: function(){}} ];
 //
 // Test getter
 //
-var x2 = {a: 1, b: 2, get c() { WScript.Echo("hello"); return 100; } };
+var x2 = {a: 1, b: 2, get c() { WScript.Echo("hello"); return 100; }, d: Math.cos(0) }; 
+
+// Adding Math.cos(0) to initialize the Math object fully, otherwise during enumerating the objects the main script context would close
+// and the enumeration would trigger the deferred type handler to undefer for the math object.
